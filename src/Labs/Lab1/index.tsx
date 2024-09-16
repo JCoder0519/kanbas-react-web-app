2a358a946e477bc1ef929929a80dc096e0b0dab0
Jcoder0519 <chen.jun@northeastern.edu>
2024-09-15 20:52:57 -0400

    heading exercise

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,6 +2,9 @@ export default function Lab1() {
     return (
         <div id="wd-lab1">
         <h2>Lab 1</h2>
+        <h3>Headings</h3>
+        <h1>H1 is the largest heading</h1>
+        <h6>H6 is the smallest heading</h6>
         </div>
     );
     }
