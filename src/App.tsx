dc184304f40ef168a67a170af1896286e21bc470
Jcoder0519 <chen.jun@northeastern.edu>
2024-09-15 20:39:52 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import Labs from "./Labs";
+
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Web Development</h1>
+      <Labs />
+    </div>
+  );
+}
+
+export default App;
